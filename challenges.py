0476e5c86c25863cfb827e233e0ba8849780862b
teknofage <teknofage@gmail.com>
2021-02-23 19:00:22 -0800

    finished lcs_dp

--- challenges.py
+++ challenges.py
@@ -26,9 +26,17 @@ def lcs_dp(strA, strB):
 
     dp_table = [[0 for j in range(cols)] for i in range(rows)]
 
-    # TODO: Fill in the table using a nested for loop.
-
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 or j == 0:
+                dp_table[i][j] = 0
+            elif strA[i - 1] == strB[j - 1]:
+                dp_table[i][j] = dp_table[i - 1][j - 1]+1
+            else:
+                dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1])
+    
     return dp_table[rows-1][cols-1]
+
 
 def knapsack(items, capacity):
     """Return the maximum value that can be stored in the knapsack using the
